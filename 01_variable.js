76b42390a1a685858cd3be1f8455b62be069ea07
Prabhash Kumar <78657057+Prabhashg@users.noreply.github.com>
2024-02-18 17:10:16 +0000

    01_variable.js 02_dataTypes.js

---
+++ 01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 12234
+let accountEmail = "abc@xyz.com"
+let accountPassword = "12122"
+
+// Don't use var as the scope for var is block scope and not functional scope
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword])
---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("This is an alert") // we are using node JS, not browser 
+
+// Data Types
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// Object
+
+console.log(typeof "hello")     // string
+console.log(typeof null)        // object
+console.log(typeof undefined)   // undefined
